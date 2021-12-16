f3e0d42e41dea35effdc7a9ebc298eceba4677ac
samsonites <samuel.son1007@gmail.com>
2021-12-17 01:58:06 +0900

    csv 읽기 수정 (사용자명, 메일, 서버이름, 서버 리소스 사요량)기반으로 메일 본문 작성

--- send-email.py
+++ send-email.py
@@ -1,28 +1,31 @@
 import smtplib
-import csv
+import csv_reader as csv
+from log import logger
 from email.mime.text import MIMEText
 import getpass
 
 sender = input('Sender Email Address : ')
 password = getpass.getpass('Password : ')
 
-smtp_info = dict({"smtp_server" : "smtp.naver.com", # SMTP 서버 주소
-                  "smtp_user_id" : sender,          # sender mail address
-                  "smtp_user_pw" : password ,       # sender mail password
-                  "smtp_port" : 587})               # SMTP 서버 포트
+smtp_info = dict({"smtp_server": "smtp.naver.com",  # SMTP 서버 주소
+                  "smtp_user_id": sender,  # sender mail address
+                  "smtp_user_pw": password,  # sender mail password
+                  "smtp_port": 587})  # SMTP 서버 포트
 
 csv_file = "sample.csv"
 
 
-# send mail                  
-def send_email(smtp_info, msg_list):
+# send emails
+def send_email(msgs):
     with smtplib.SMTP(smtp_info["smtp_server"], smtp_info["smtp_port"]) as server:
+        logger.info("getting credential for login")
         # TLS 보안 연결
-        server.starttls() 
+        server.starttls()
         # 로그인
         server.login(smtp_info["smtp_user_id"], smtp_info["smtp_user_pw"])
+        logger.info("login succeeded")
 
-        for msg in msg_list:
+        for msg in msgs:
             # 로그인 된 서버에 이메일 전송(메시지를 보낼때는 .as_string() 메소드를 사용해서 문자열로 바꿔줍니다.)
             response = server.sendmail(msg['from'], msg['to'], msg.as_string())
 
@@ -32,31 +35,7 @@ def send_email(smtp_info, msg_list):
             else:
                 print(response)
 
-# create mailing list from csv file
-def create_mailing_list_from_csv(file_name) : 
-       
-    msg_list = []
 
-    f = open(file_name, 'r')
-    reader = csv.DictReader(f)
-    for row in reader:
-        # get email info off of csv
-        title   = row['title']
-        content = row['content']
-        receiver= row['mail']
-
-        msg = MIMEText(_text = content, _charset = "utf-8") # 메일 내용
-
-        msg['Subject'] = title     # 메일 제목
-        msg['From'] = sender       # 송신자
-        msg['To'] = receiver       # 수신자
-
-        msg_list.append(msg)
-
-    return msg_list
-                
-## main logic
-
-msg_list = create_mailing_list_from_csv(csv_file)
-send_email(smtp_info, msg_list )
-
+# main logic
+msg_list = csv.create_mailing_list_from_csv(sender, csv_file)
+send_email(msg_list)
